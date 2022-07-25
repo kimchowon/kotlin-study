f0e40334841bdcbf446f71e6742144c75671ee6d
김초원 <zzzcc33@naver.com>
2022-07-25 21:51:53 +0900

    코드를 다루는 방법 - 반복문을 다루는 방법

--- src/main/java/study_2/study_2_2/Test01_Java.java
+++
@@ -1,4 +0,0 @@
-package study_2.study_2_2;
-
-public class Test01_Java {
-}
--- src/main/kotlin/study_2/study_2_2/Test01.kt
+++ src/main/kotlin/study_2/study_2_2/Test01.kt
@@ -8,3 +8,34 @@ package study_2.study_2_2
  */
 class Test01 {
 }
+
+fun main(args: Array<String>) {
+
+    // 1.for-each문
+    val numbers = listOf(1L, 2L, 3L);
+    for (number in numbers) {
+        println(number)
+    }
+
+    // 2.for문 오름차순
+    for (i in 1..3) {
+        println(i)
+    }
+
+    // 3.for문 내림차순
+    for (i in 3 downTo 1) {
+        println(i)
+    }
+
+    // 4.for문 2씩 증가
+    for (i in 1..5 step 2) {
+        println(i)
+    }
+
+    // 5.while문
+    var i = 0
+    while (i <= 3) {
+        println(i)
+        i++;
+    }
+}
